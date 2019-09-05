572ee7bed12bac0e9795faa8b26e81ad6c2feee3
nearnshaw <nearnshaw@decentraland.org>
2019-09-05 15:15:55 -0300

    multiple buttons

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,5 @@
 let textOffset = new Transform({
-	position: new Vector3(0, 1, 0)
+	position: new Vector3(0, 1.5, 0)
   })
 
 function addLabel(text: string, parent: IEntity){
@@ -18,6 +18,15 @@ let greenMaterial = new Material()
 greenMaterial.albedoColor = Color3.Green()
 
 
+
+// ground
+let floor = new Entity()
+floor.addComponent(new GLTFShape("models/FloorBaseGrass.glb"))
+floor.addComponent(new Transform({
+  position: new Vector3(8, 0, 8),
+  scale:new Vector3(1.6, 0.1, 1.6)
+}))
+engine.addEntity(floor)
 
 
 // Click
@@ -110,22 +119,6 @@ function distance(pos1: Vector3, pos2: Vector3): number {
 /////////Global pointerdown
 
 
-// Instance the input object
-const input = Input.instance
-
-// button down event
-input.subscribe("BUTTON_DOWN", e => {
-  log("button A Down", e)
-  globalPointerDownCube.addComponentOrReplace(greenMaterial)
-})
-
-// button up event
-input.subscribe("BUTTON_UP", e => {
-  log("button A Up", e)
-  globalPointerUpCube.addComponentOrReplace(greenMaterial)
-})
-
-
 
 
 // Global Pointer Down
@@ -137,7 +130,7 @@ globalPointerDownCube.addComponent(new Transform({
 globalPointerDownCube.addComponent(new SphereShape())
 engine.addEntity(globalPointerDownCube)
 
-addLabel("Global down", globalPointerDownCube)
+addLabel("Global pointer down", globalPointerDownCube)
 
 
 
@@ -150,54 +143,222 @@ globalPointerUpCube.addComponent(new Transform({
 globalPointerUpCube.addComponent(new SphereShape())
 engine.addEntity(globalPointerUpCube)
 
-addLabel("Global up", globalPointerUpCube)
+addLabel("Global pointer up", globalPointerUpCube)
 
 
 
-/////// ANIMATIONS
-
-// Play sit animation
-let idleAnimation = new AnimationState('Idle')
-let sittingAnimation = new AnimationState('Sitting')
-sittingAnimation.looping = false
-
-const dog = new Entity()
-dog.addComponent(new GLTFShape('models/BlockDog.glb'))
-dog.addComponent(new Transform({
-  position: new Vector3(10, 0, 10)
+// Global Primary Down
+let globalPrimaryDownCube = new Entity()
+globalPrimaryDownCube.addComponent(new Transform({
+  position: new Vector3(4, 1, 10),
+  scale: new Vector3(0.5, 0.5, 0.5)
 }))
-let dogAnim = new Animator()
-idleAnimation.playing = true
-dogAnim.addClip(idleAnimation)
-dogAnim.addClip(sittingAnimation)
+globalPrimaryDownCube.addComponent(new SphereShape())
+engine.addEntity(globalPrimaryDownCube)
 
-dog.addComponent(dogAnim)
-engine.addEntity(dog)
+addLabel("Global primary down", globalPrimaryDownCube)
 
 
 
-let sitCube = new Entity()
-sitCube.addComponent(new Transform({
+// Global Primary Up
+let globalPrimaryUpCube = new Entity()
+globalPrimaryUpCube.addComponent(new Transform({
+  position: new Vector3(4, 1, 12),
+  scale: new Vector3(0.5, 0.5, 0.5)
+}))
+globalPrimaryUpCube.addComponent(new SphereShape())
+engine.addEntity(globalPrimaryUpCube)
+
+addLabel("Global primary up", globalPrimaryUpCube)
+
+
+
+// Global Secondary Down
+let globalSecondaryDownCube = new Entity()
+globalSecondaryDownCube.addComponent(new Transform({
+  position: new Vector3(6, 1, 10),
+  scale: new Vector3(0.5, 0.5, 0.5)
+}))
+globalSecondaryDownCube.addComponent(new SphereShape())
+engine.addEntity(globalSecondaryDownCube)
+
+addLabel("Global secondary down", globalSecondaryDownCube)
+
+
+
+// Global Secondary Up
+let globalSecondaryUpCube = new Entity()
+globalSecondaryUpCube.addComponent(new Transform({
+  position: new Vector3(6, 1, 12),
+  scale: new Vector3(0.5, 0.5, 0.5)
+}))
+globalSecondaryUpCube.addComponent(new SphereShape())
+engine.addEntity(globalSecondaryUpCube)
+
+addLabel("Global secondary up", globalSecondaryUpCube)
+
+
+
+
+
+
+
+
+
+
+//  Primary Down
+let primaryDownCube = new Entity()
+primaryDownCube.addComponent(new Transform({
+  position: new Vector3(8, 1, 12)
+}))
+primaryDownCube.addComponent(new BoxShape())
+engine.addEntity(primaryDownCube)
+
+addLabel("Primary down", primaryDownCube)
+
+
+
+// Primary Up
+let primaryUpCube = new Entity()
+primaryUpCube.addComponent(new Transform({
+  position: new Vector3(10, 1, 12)
+}))
+primaryUpCube.addComponent(new BoxShape())
+engine.addEntity(primaryUpCube)
+
+addLabel("Primary up", primaryUpCube)
+
+
+
+// Secondary Down
+let secondaryDownCube = new Entity()
+secondaryDownCube.addComponent(new Transform({
   position: new Vector3(12, 1, 12)
 }))
-sitCube.addComponent(new BoxShape())
-sitCube.addComponent(new OnClick(e => {
-  sitCube.addComponentOrReplace(greenMaterial)
-  idleAnimation.stop()
-  sittingAnimation.reset()
-  sittingAnimation.play()
+secondaryDownCube.addComponent(new BoxShape())
+engine.addEntity(secondaryDownCube)
+
+addLabel("Secondary down", secondaryDownCube)
+
+
+
+// Secondary Up
+let secondaryUpCube = new Entity()
+secondaryUpCube.addComponent(new Transform({
+  position: new Vector3(14, 1, 12)
 }))
-engine.addEntity(sitCube)
+secondaryUpCube.addComponent(new BoxShape())
+engine.addEntity(secondaryUpCube)
 
-addLabel("Play sit", sitCube)
+addLabel("Secondary up", secondaryUpCube)
 
 
-// ground
-let floor = new Entity()
-floor.addComponent(new GLTFShape("models/FloorBaseGrass.glb"))
-floor.addComponent(new Transform({
-  position: new Vector3(8, 0, 8),
-  scale:new Vector3(1.6, 0.1, 1.6)
+// robots base
+const robots = new Entity()
+robots.addComponent(new GLTFShape('models/Robots.glb'))
+robots.addComponent(new Transform({
+  position: new Vector3(12, 0, 2)
 }))
-engine.addEntity(floor)
+engine.addEntity(robots)
 
+robots.addComponent(new OnPointerDown(e => {
+	log(e.hit.meshName)
+	if (e.hit.meshName == "Droid_01"){
+		robot1Cube.addComponentOrReplace(greenMaterial)
+	} else if (e.hit.meshName == "Droid_02"){
+		robot2Cube.addComponentOrReplace(greenMaterial)
+	}
+}))
+
+
+// Robot1
+let robot1Cube = new Entity()
+robot1Cube.addComponent(new Transform({
+  position: new Vector3(13, 1, 1.5),
+  scale: new Vector3(0.5, 0.5, 0.5)
+}))
+robot1Cube.addComponent(new BoxShape())
+engine.addEntity(robot1Cube)
+
+addLabel("Click robot 1", robot1Cube)
+
+
+// Robot2
+let robot2Cube = new Entity()
+robot2Cube.addComponent(new Transform({
+  position: new Vector3(10.5, 1, 1.5),
+  scale: new Vector3(0.5, 0.5, 0.5)
+}))
+robot2Cube.addComponent(new BoxShape())
+engine.addEntity(robot2Cube)
+
+addLabel("Click robot 2", robot2Cube)
+
+
+
+
+// Instance the input object
+const input = Input.instance
+
+// pointer down event
+input.subscribe("BUTTON_DOWN", ActionButton.POINTER, true, e => {
+  log("pointer down", e)
+  if(e.hit.entityId == pointerDownCube.uuid){
+	pointerDownCube.addComponentOrReplace(greenMaterial)
+  } else {
+	globalPointerDownCube.addComponentOrReplace(greenMaterial)
+  }
+})
+
+// pointer up event
+input.subscribe("BUTTON_UP", ActionButton.POINTER, true, e => {
+	log("pointer down", e)
+	if(e.hit.entityId == pointerUpCube.uuid){
+		pointerUpCube.addComponentOrReplace(greenMaterial)
+	} else {
+		globalPointerUpCube.addComponentOrReplace(greenMaterial)
+	}
+})
+
+
+// primary down event
+input.subscribe("BUTTON_DOWN", ActionButton.PRIMARY, true, e => {
+	log("primary down", e)
+	if(e.hit.entityId == primaryDownCube.uuid){
+		primaryDownCube.addComponentOrReplace(greenMaterial)
+	} else {
+	  globalPrimaryDownCube.addComponentOrReplace(greenMaterial)
+	}
+  })
+
+// primary up event
+input.subscribe("BUTTON_UP", ActionButton.PRIMARY, true, e => {
+	log("primary down", e)
+	if(e.hit.entityId == primaryUpCube.uuid){
+		primaryUpCube.addComponentOrReplace(greenMaterial)
+	} else {
+		globalPrimaryUpCube.addComponentOrReplace(greenMaterial)
+	}
+})
+
+
+
+// secondary down event
+input.subscribe("BUTTON_DOWN", ActionButton.SECONDARY, true, e => {
+	log("secondary down", e)
+	if(e.hit.entityId == secondaryDownCube.uuid){
+		secondaryDownCube.addComponentOrReplace(greenMaterial)
+	} else {
+	  globalSecondaryDownCube.addComponentOrReplace(greenMaterial)
+	}
+  })
+  
+// secondary up event
+input.subscribe("BUTTON_UP", ActionButton.SECONDARY, true, e => {
+	log("secondary down", e)
+	if(e.hit.entityId == secondaryUpCube.uuid){
+		secondaryUpCube.addComponentOrReplace(greenMaterial)
+	} else {
+		globalSecondaryUpCube.addComponentOrReplace(greenMaterial)
+	}
+})
