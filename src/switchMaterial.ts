ba1d56e9b1b611b4edc369b9a77b95f70e041c45
nearnshaw <nearnshaw@decentraland.org>
2020-10-09 17:32:46 -0300

    fix repeated component bug

--- src/switchMaterial.ts
+++ src/switchMaterial.ts
@@ -1,19 +1,17 @@
-
-import utils from "../node_modules/decentraland-ecs-utils/index"
-
+import utils from '../node_modules/decentraland-ecs-utils/index'
 
 // reusable materials
 let greenMaterial = new Material()
 greenMaterial.albedoColor = Color3.Green()
 
 let lightGreenMaterial = new Material()
-lightGreenMaterial.albedoColor = Color3.FromHexString("#abe8bd")
+lightGreenMaterial.albedoColor = Color3.FromHexString('#abe8bd')
 
-
-export function activate(entity: Entity){
-	entity.addComponentOrReplace(greenMaterial)
-	entity.addComponent(new utils.Delay(1000,
-		()=>{
-			entity.addComponentOrReplace(lightGreenMaterial)
-		}))
+export function activate(entity: Entity) {
+  entity.addComponentOrReplace(greenMaterial)
+  entity.addComponentOrReplace(
+    new utils.Delay(1000, () => {
+      entity.addComponentOrReplace(lightGreenMaterial)
+    })
+  )
 }
